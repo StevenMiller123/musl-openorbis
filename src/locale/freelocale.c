2fe3f8ff28116ac5693f92f089a35ea1c6434bfe
Stephen Miller <56742918+StevenMiller123@users.noreply.github.com>
2026-08-29 19:24:41 -0500

    oops 2

--- src/locale/freelocale.c
+++ src/locale/freelocale.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include "locale_impl.h"
 
-void freelocale(locale_t l)
+int freelocale(locale_t l)
 {
 	if (__loc_is_allocated(l)) free(l);
+	return 0;
 }
 
 weak_alias(freelocale, __freelocale);
